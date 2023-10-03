c33e0483a28ef2c55485e7eb313049f286a45b47
alpergh <bilginalper180@gmail.com>
2023-10-03 17:24:47 +0300

    git bub

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+facts_list = ["Elon Musk, sosyal ağların içeriği görüntülemek için mümkün olduğunca fazla zaman harcamamız için bizi platformun içinde tutmak üzere tasarlandığını iddia ediyor.",
+            "2018 yılında yapılan bir araştırmaya göre 18-34 yaş arası kişilerin %50'den fazlası akıllı telefonlarına bağımlı olduğunu düşünüyor.",
+            "Sosyal ağların olumlu ve olumsuz yanları var ve bu platformları kullanırken her ikisinin de farkında olmalıyız.",
+            "Teknoloji bağımlılığı çalışması, modern bilimsel araştırmanın en alakalı alanlarından biridir."]
+@app.route("/")
+def hello_world():
+   return f'<h1>selamın aleyküm</h1><a href ="/rastgelebilgi">Rastgele bilgi için tıklayın</a>'
+
+
+@app.route("/rastgelebilgi")
+def inform_me():
+    return f'<h1>{random.choice(facts_list)}, Daha fazlası için sayfayı yenileyin.</h1>'
+
+@app.route("/ADRES")
+def FONKSIYON_ADI():
+    
+app.run(debug=True)
